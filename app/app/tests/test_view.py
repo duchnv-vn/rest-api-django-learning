18b00f5da29d9a07c812bd68b0c3550a7e43f812
duchnv-vn <huynhnguyenvietduc@gmail.com>
2024-01-18 17:02:59 +0700

    Create tests dir

---
+++ app/app/tests/test_view.py
@@ -0,0 +1,13 @@
+from django.test import SimpleTestCase
+from app import calc
+from rest_framework.test import APIClient
+
+
+class TestViews(SimpleTestCase):
+    client = APIClient()
+
+    def test_get_greetings(self):
+        res = self.client.get('/greetings/')
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.data, ['Hello', 'Ciao', 'Xin chao'])
