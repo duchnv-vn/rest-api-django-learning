b7c295cd0fc3455194f9e499a3d8070a3573fc0a
duchnv-vn <huynhnguyenvietduc@gmail.com>
2024-02-01 22:56:39 +0700

    Deployment: Configure production container build flow

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,13 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-v3ad!qub)i1ut@(t$vjgf%059)q8x4#y)17$v_t44t^q=m6afp"
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 # Application definition
 
@@ -81,14 +86,13 @@ WSGI_APPLICATION = "app.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "HOST": os.environ.get("DB_HOST"),
         "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", 'changeme'),
     }
 }
 
--- scripts/run.sh
+++ scripts/run.sh
@@ -8,4 +8,8 @@ python manage.py collectstatic --noinput
 
 python manage.py migrate
 
-uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
+uwsgi --socket :9000 \
+    --workers 4 \
+    --master \
+    --enable-threads \
+    --module app.wsgi
