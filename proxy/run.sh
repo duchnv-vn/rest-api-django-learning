db7f8eb15ff60e8ee20619d9a02df53005ec1a0e
duchnv-vn <huynhnguyenvietduc@gmail.com>
2024-03-03 21:08:05 +0700

    Config Nginx SSL

--- proxy/run.sh
+++ proxy/run.sh
@@ -3,6 +3,6 @@
 set -e
 
 # envsubst </etc/nginx/default.conf.tpl >/etc/nginx/conf.d/default.conf
-envsubst </etc/nginx/nginx.conf.tpl >/etc/nginx/conf.d/nginx.conf
+envsubst </etc/nginx/nginx.conf.tpl >/etc/nginx/conf.d/default.conf
 
 nginx -g 'daemon off;'
